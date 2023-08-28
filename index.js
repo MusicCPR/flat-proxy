4f57076e0048043897cab0dbf8a79b1b27011c70
Michael Stewart <tgm@vt.edu>
2023-08-28 09:08:35 -0400

    remove commented

--- index.js
+++ index.js
@@ -19,30 +19,6 @@ app.use(
 
 app.use("/", express.static("public"));
 
-// app.get("/:scoreId", (req, res) => {
-//   console.log('reqUrl' , req.url)
-//   console.log('got request for scoreId: ' , req.params.scoreId)
-//   const qs = req.url.substring(req.url.indexOf('?'))
-//   console.log('qs', qs)
-//   https
-//     .get(`https://flat-embed.com${qs}`, (resp) => {
-//       let data = "";
-
-//       // A chunk of data has been received.
-//       resp.on("data", (chunk) => {
-//         data += chunk;
-//       });
-
-//       // The whole response has been received. Print out the result.
-//       resp.on("end", () => {
-//         console.log(JSON.parse(data).explanation);
-//       });
-//     })
-//     .on("error", (err) => {
-//       console.log("Error: " + err.message);
-//     });
-//   // res.send("Hello World!");
-// });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
