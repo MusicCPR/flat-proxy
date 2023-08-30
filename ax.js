7c9470f3a1270bce599711dba1d27d70c259208a
Michael Stewart <tgm@vt.edu>
2023-08-30 01:09:00 -0400

    almooooooooost

--- ax.js
+++ ax.js
@@ -2,6 +2,8 @@ const axios = require("axios").default;
 const express = require("express");
 const app = express();
 const port = 3000;
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 app.use("/", express.static("public"));
 
@@ -10,11 +12,28 @@ const flatUrl = 'https://flat-embed.com'
 app.get('/proxy/:scoreId', (req, res) => {
   const qs = req.url.substring(req.url.indexOf("?"));
   var resolvedPathValue = `/${req.params.scoreId}${qs}`;
-  console.log('req.url')
-  console.log(req.url)
-  axios.get(`${flatUrl}${resolvedPathValue}`).then(res.send);
+  axios.get(`${flatUrl}${resolvedPathValue}`).then((proxied) => {
+    res.set(proxied.headers).send(proxied.data)
+  });
 })
 
+app.post('*', (req, res) => {
+  const qs = req.url.substring(req.url.indexOf("?"));
+  var resolvedPathValue = `/${qs}`;
+  axios.post(`${flatUrl}${resolvedPathValue}`, req.body
+  ).then((proxied) => {
+    
+    res.set(proxied.headers).send(proxied.data)
+  });
+})
+
+app.get("*", (req, res) => {
+  const qs = req.url.substring(req.url.indexOf("?"));
+  var resolvedPathValue = `/${qs}`;
+  axios.get(`${flatUrl}${resolvedPathValue}`).then((proxied) => {
+    res.set(proxied.headers).send(proxied.data);
+  });
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
 });
